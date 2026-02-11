71853e004a370d9764ff3adc1650eff0617f61ee
shmuel2222 <shmuelshmuel202@gmail.com>
2026-02-11 10:01:30 -0800

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ window.onload = function () {
     const wheel = document.getElementById("wheel");
     const spinBtn = document.getElementById("spin");
 
+    if (!wheel || !spinBtn) {
+        console.log("Element not found");
+        return;
+    }
+
     let isSpinning = false;
 
     spinBtn.addEventListener("click", function () {
