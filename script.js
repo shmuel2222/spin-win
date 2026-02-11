d3f3301331dc39487cfcc50ea8c7f0f4cd09b364
shmuel2222 <shmuelshmuel202@gmail.com>
2026-02-10 17:41:11 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-const wheel = document.getElementById("wheel");
-const btn = document.getElementById("spinBtn");
-
-let spinning = false;
-
-btn.onclick = () => {
-  if (spinning) return;
-  spinning = true;
-
-  const deg = Math.floor(3000 + Math.random() * 2000);
-  wheel.style.transform = rotate(${deg}deg);
-
-  setTimeout(() => {
-    spinning = false;
-  }, 4000);
-};
+alert("JS is working");
